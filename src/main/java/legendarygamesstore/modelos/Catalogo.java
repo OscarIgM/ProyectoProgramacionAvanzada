3b796ce090363f1703135134b1901a2c10626af2
OscarIgM <90069685+OscarIgM@users.noreply.github.com>
2023-06-27 10:44:38 -0400

    Committ PreFinalizacion Refactoring

--- src/main/java/legendarygamesstore/modelos/Catalogo.java
+++ src/main/java/legendarygamesstore/modelos/Catalogo.java
@@ -1,26 +1,31 @@
 package legendarygamesstore.modelos;
 
+import legendarygamesstore.servicio.ClienteControlador;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class Catalogo {
-    private List<Videojuego> Catalogo = new ArrayList<>();
+    private List<Videojuego> catalogo = new ArrayList<>();
+    private static final Logger logger=Logger.getLogger(Catalogo.class.getName());
+
     public void mostrarInventario() {
         for (int i = 0; i < getCatalogo().size(); i++) {
-            System.out.println(this.getCatalogo().get(i).getTitulo());
+            logger.info(this.getCatalogo().get(i).getTitulo());
         }
     }
-public void añadirInventario(Videojuego videojuego){
-    this.Catalogo.add(videojuego);
+public void agregarInventario(Videojuego videojuego){
+    this.catalogo.add(videojuego);
 }
 public void eliminarInventario(Videojuego videojuego){
-    this.Catalogo.remove(videojuego);
+    this.catalogo.remove(videojuego);
 }
 }
