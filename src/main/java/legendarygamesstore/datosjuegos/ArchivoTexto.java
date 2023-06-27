db58105f6cf8a65bbabde993514cbfdb002986bb
OscarIgM <90069685+OscarIgM@users.noreply.github.com>
2023-06-27 10:49:02 -0400

    Committ PreFinalizacion Refactoring

--- src/main/java/legendarygamesstore/datosjuegos/ArchivoTexto.java
+++ src/main/java/legendarygamesstore/datosjuegos/ArchivoTexto.java
@@ -1,4 +1,5 @@
 package legendarygamesstore.datosjuegos;
+
 import legendarygamesstore.modelos.Cliente;
 import legendarygamesstore.modelos.Videojuego;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -10,9 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArchivoTexto  {
+    static final String datosClientes="DatosClientes";
+    static final String datosJuegos="DatosJuegos";
+
     public void registrarCliente(Cliente cliente) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File archivo = new File("DatosClientes");
+        File archivo = new File(datosClientes);
         if (archivo.exists()) {
             List<Cliente> listaObjetos = objectMapper.readValue(archivo, new TypeReference<List<Cliente>>() {
             });
@@ -26,7 +30,7 @@ public class ArchivoTexto  {
     public List<Cliente> obtenerClientesDesdeJSON() {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(new File("DatosClientes"), objectMapper.getTypeFactory().constructCollectionType(List.class, Cliente.class));
+            return objectMapper.readValue(new File(datosClientes), objectMapper.getTypeFactory().constructCollectionType(List.class, Cliente.class));
         } catch (JsonParseException | JsonMappingException e) {
             System.out.println("Ocurrió un error al parsear el JSON: " + e.getMessage());
         } catch (IOException e) {
@@ -36,7 +40,7 @@ public class ArchivoTexto  {
     }
     public void actualizarCliente(Cliente cliente) {
         ObjectMapper objectMapper = new ObjectMapper();
-        File archivo = new File("DatosClientes");
+        File archivo = new File(datosClientes);
         try {
             if (archivo.exists()) {
                 List<Cliente> listaObjetos = objectMapper.readValue(archivo, new TypeReference<List<Cliente>>() {});
@@ -53,24 +57,9 @@ public class ArchivoTexto  {
         }
     }
 
-
-
-    public void crearArchivoJSON(String nombreArchivo) {
-        File archivo = new File(nombreArchivo);
-        if (!archivo.exists()) {
-            try {
-                archivo.createNewFile();
-                System.out.println("Archivo JSON creado correctamente.");
-            } catch (IOException e) {
-                System.out.println("Ocurrió un error al crear el archivo JSON: " + e.getMessage());
-            }
-        } else {
-            System.out.println("El archivo JSON ya existe.");
-        }
-    }
     public List<Videojuego> leerJsonVideojuego() {
         ObjectMapper objectMapper = new ObjectMapper();
-        File archivo = new File("DatosJuegos");
+        File archivo = new File(datosJuegos);
         try {
             if (archivo.exists()) {
                 return objectMapper.readValue(archivo, new TypeReference<List<Videojuego>>() {});
@@ -83,23 +72,7 @@ public class ArchivoTexto  {
             return null;
         }
     }
-    public Cliente obtenerClientePorNombre(String nombreUsuario) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        File archivo = new File("DatosClientes");
-        try {
-            List<Cliente> listaClientes = objectMapper.readValue(archivo, new TypeReference<List<Cliente>>() {});
-            for (Cliente cliente : listaClientes) {
-                if (cliente.getNombreUsuario().equals(nombreUsuario)) {
-                    return cliente;
-                }
-            }
-            System.out.println("No se encontró un cliente con el nombre de usuario: " + nombreUsuario);
-        } catch (IOException e) {
-            System.out.println("Error al leer el archivo de clientes.");
-            e.printStackTrace();
-        }
-        return null;
-    }
+
 }
 
 
